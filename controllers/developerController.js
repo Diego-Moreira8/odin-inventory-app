870f3bdc3f78770accde3ede2fc5bbee6a04291e
Diego Moreira de Oliveira <diego.moreira529@gmail.com>
2024-07-25 14:34:40 -0300

    feat: developer page controller and view

--- controllers/developerController.js
+++ controllers/developerController.js
@@ -1,9 +1,19 @@
 const asyncHandler = require("express-async-handler");
 
 const Developer = require("../models/Developer");
+const Game = require("../models/Game");
 
 exports.all_developers = asyncHandler(async (req, res, next) => {
   const allDevelopers = await Developer.find().exec();
 
   res.render("developers", { allDevelopers: allDevelopers });
+});
+
+exports.developer_page = asyncHandler(async (req, res, next) => {
+  const [developer, allGames] = await Promise.all([
+    await Developer.findById(req.params.id).exec(),
+    await Game.find({ developer: req.params.id }).populate("genre").exec(),
+  ]);
+
+  res.render("developerPage", { developer: developer, allGames: allGames });
 });
--- routes/index.js
+++ routes/index.js
@@ -43,6 +43,9 @@ router.get("/jogo/:id", game_controller.game_details);
 // GET developers
 router.get("/desenvolvedores", developer_controller.all_developers);
 
+// GET developer by id
+router.get("/desenvolvedor/:id", developer_controller.developer_page);
+
 /// GENRES ROUTES ///
 
 // GET genres
